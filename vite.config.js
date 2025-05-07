2b418bdc5746cf6d31a5260b210e75b25c03388e
Mukul Sauhta <msauhta2@ncsu.edu>
2025-05-06 23:30:23 -0400

    trying to get it working on github pages once more

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/beesbridge-remake/',
   plugins: [react()],
+  base: '/beesbridge-remake/',
 })
